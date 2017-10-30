480328d96b43d1d16895b3e7335ac747a00a3dee
BryanOrabutt <borabut@siue.edu>
2017-10-30 13:30:30 -0500

    Update programmer_top.v

--- PGM8755/src/programmer_top.v
+++ PGM8755/src/programmer_top.v
@@ -7,6 +7,30 @@
 // Project Name:   	PGM8755
 // Target Devices: 	Spartan 3E Starter Board, Mojo board (Spartan 6 xc6slx9-2tqg144)
 // Description:  		Buffer to hold instruction bytes while EPROM is being programmed.
+// Portlist:		clk - 50 MHz clock for all sequential eleents. (slower clocks derived
+// 						in individual modules)
+//					rst_n - low active reset button on board.
+//					rx - UART receive line
+//					mode - determines if FPGA is writing data or reading data. Should be
+//						connected to a toggle switch.
+//					en - enables/disables the programmer element. UART and Buffer reamin
+//						active. Use to write to buffer before enabling programmer.
+//						Connect this line to a toggle switch.
+//					tx - UART transmit line
+//					pce - 8755 prog/ce line control. 0 = CE state, 1 = Prog sate
+//					rd - 8755 low active RD line. Used when verifying EPROM.
+//					vdd_25 - +25V control line. 8755 VDD must be held at +25V while
+//						latching data. This is a low active line to connect to a
+//						PFET high side switch to +25V.
+//					vdd_5 - +5V control line. 8755 VDD must be held at 5V while NOT
+//						latching data. This is a low active line to connect to a
+//						PFET high side switch to +5V.
+//					ale - Address Latch Enable. Latches address into EPROM before
+//						writing data.
+//					led[1:0] - LED0 = RX indicator, LED1 = TX indicator
+//					address - Multiplexed Address/data line. represents 10-bit
+//						address when ALE is active, and represents 8-bit data
+//						when data is being latched or read.
 //////////////////////////////////////////////////////////////////////////////////
 module programmer_top(input clk, rst_n, rx, mode, en, output tx, pce, rd, vdd_25, vdd_5, 
 					  ale, output[1:0]led, output[10:0] address);
@@ -18,7 +42,6 @@ module programmer_top(input clk, rst_n, rx, mode, en, output tx, pce, rd, vdd_25
 	 * new_tx_data: flag to indicate data is ready to be transmitted.
 	 * busy: inidcates transmitter is in process of sending a byte
 	 * block: forces transmitter into busy state.
-	 * pce: program/ce for 8755. 0 = CE state 1 = prog state.
 	 */
 	wire new_rx_data, new_tx_data, busy, block;
 	
